50f7f78522190077d7855da4ac44671c9d28c848
rayyyy <nraavy@gmail.com>
2022-06-01 15:01:54 +0900

    typeorm

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -9,18 +9,21 @@ export class UserController {
     return this.userRepository.find()
   }
 
-  // async one(request: Request, response: Response, next: NextFunction) {
-  //   return this.userRepository.findOne(request.params.id)
-  // }
+  async one(request: Request, response: Response, next: NextFunction) {
+    return this.userRepository.findOneBy({ id: Number(request.params.id) })
+  }
 
   async save(request: Request, response: Response, next: NextFunction) {
     return this.userRepository.save(request.body)
   }
 
-  // async remove(request: Request, response: Response, next: NextFunction) {
-  //   let userToRemove = await this.userRepository.findOneBy({
-  //     id: request.params.id,
-  //   })
-  //   await this.userRepository.remove(userToRemove)
-  // }
+  async remove(request: Request, response: Response, next: NextFunction) {
+    let userToRemove = await this.userRepository.findOneBy({
+      id: Number(request.params.id),
+    })
+    if (userToRemove !== null) {
+      await this.userRepository.remove(userToRemove)
+    }
+    return
+  }
 }
